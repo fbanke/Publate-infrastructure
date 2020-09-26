c82822336bff29ac6f20925ea9f585550d10740a
Frederik Banke <frederik@patch.dk>
2020-09-26 20:30:00 +0200

    Add api domain in DNS

---
+++ main.tf
@@ -0,0 +1,15 @@
+﻿provider "aws" {
+  region = var.region
+}
+
+resource "aws_route53_zone" "primary" {
+  name = var.dns_zone
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = var.api_domain
+  type    = "CNAME"
+  ttl     = "300"
+  records = [var.load_balancer]
+}

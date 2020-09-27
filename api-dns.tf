8e4525cbc20d6a6efd55733dbca7ab4869c8fe85
Frederik Banke <frederik@patch.dk>
2020-09-27 21:27:22 +0200

    Added local. domain to support integration testing

--- api-dns.tf
+++ api-dns.tf
@@ -2,10 +2,17 @@
   name = var.dns_zone
 }
 
-resource "aws_route53_record" "www" {
+resource "aws_route53_record" "api" {
   zone_id = aws_route53_zone.primary.zone_id
   name    = var.api_domain
   type    = "CNAME"
   ttl     = "300"
   records = [var.load_balancer]
+}
+
+resource "aws_route53_record" "local" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = "local.${var.dns_zone}"
+  type    = "A"
+  records = ["127.0.0.1"]
 }
